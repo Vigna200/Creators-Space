b2044b63c447c3dfaeba4ebfe58667e952070749
Kamna Sharma <bca_06317702024_kamna@vipstc.edu.in>
2025-07-30 14:22:17 +0530

    Fix:dark mode using localStorage

--- src/js/utils.js
+++ src/js/utils.js
@@ -6,3 +6,23 @@ function toggleDarkMode()
     element.classList.toggle("dark")
 
 }
+
+function toggleDarkMode() {
+    const element = document.getElementById("main-body");
+
+    element.classList.toggle("dark");
+
+    if(element.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+    } else{
+        localStorage.setItem("theme","light");
+    }
+}
+
+
+ if (localStorage.getItem("theme") === "dark") {
+      document.addEventListener("DOMContentLoaded", function () {
+        document.getElementById("main-body")?.classList.add("dark");
+      });
+ }
+
